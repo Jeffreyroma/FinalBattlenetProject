28909b123c6ad7678e3a311a56a83c3aeedfce4d
Jeffreyroma <romamakarcikov@gmail.com>
2023-06-17 19:00:54 +0300

    add some for battle test class

--- src/test/java/BattleTest.java
+++ src/test/java/BattleTest.java
@@ -1,15 +1,28 @@
+import net.Battle.driver.DriverManager;
+import net.Battle.steps.AuthorizationFormStep;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class BattleTest {
 
+    protected WebDriver driver;
+
+    private AuthorizationFormStep authorizationFormStep;
+
+
+
     @BeforeClass
     public void preparationBattleTest() {
-
+        driver = DriverManager.getDriver();
+        authorizationFormStep = new AuthorizationFormStep(driver);
     }
 
     @Test
     public void checkAuthorizationFormData() {
+        authorizationFormStep.enterEmail("romamakarcikov@gmail.com");
+        authorizationFormStep.enterPassword("Bastardjeffrey123");
+        authorizationFormStep.clickSubmitButton();
 
     }
 }
