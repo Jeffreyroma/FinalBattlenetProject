2d99c833fe8ca502600d45d49187cfd35f497c5d
Jeffreyroma <romamakarcikov@gmail.com>
2023-06-17 18:29:30 +0300

    Add class userData and battleTest, add properties

---
+++ src/test/java/BattleTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class BattleTest {
+
+    @BeforeClass
+    public void preparationBattleTest() {
+
+    }
+
+    @Test
+    public void checkAuthorizationFormData() {
+
+    }
+}
